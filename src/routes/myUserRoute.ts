b1c8141f7db30c1bac526ff70d8e9cebb694c35c
Marvinkwame <marvinkudjo@gmail.com>
2024-04-03 01:59:41 +0000

    Initial Commit

---
+++ src/routes/myUserRoute.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import myUserController from "../controllers/myUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
+
+const router = express.Router();
+
+router.get("/", jwtCheck, jwtParse, myUserController.getCurrentUser);
+router.post("/", jwtCheck, myUserController.createCurrentUser)
+router.patch("/", jwtCheck, jwtParse, validateMyUserRequest, myUserController.updateCurrentUser)
+
+export default router;
