bcf1e570a918d9cb6c99ee73b7ef6e7fe2c2bd9a
Marvinkwame <marvinkudjo@gmail.com>
2024-06-20 23:22:43 +0000

    Get Restaurant By Id Endpoint

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -1,80 +1,93 @@
-import { Request, Response } from 'express'
-import Restaurant from '../models/Restaurant'
-
+import { Request, Response } from "express";
+import Restaurant from "../models/Restaurant";
 
 const searchRestaurant = async (req: Request, res: Response) => {
-    try {
-        const city = req.params.city;
-        const searchQuery = (req.query.searchQuery as string) || "";
-        const selectedCuisines = req.query.selectedCuisines as string || "";
-        const sortOption = req.query.sortOption as string || "lastUpdated";
-        const page = parseInt(req.query.page as string) || 1;
+  try {
+    const city = req.params.city;
+    const searchQuery = (req.query.searchQuery as string) || "";
+    const selectedCuisines = (req.query.selectedCuisines as string) || "";
+    const sortOption = (req.query.sortOption as string) || "lastUpdated";
+    const page = parseInt(req.query.page as string) || 1;
 
-        let query: any = {};
+    let query: any = {};
 
-
-        query["city"] = new RegExp(city, "i")
-        const cityCheck = await Restaurant.countDocuments(query)
-        if (cityCheck === 0) {
-            return res.status(404).json({
-                data: [],
-                pagination: {
-                    total: 0,
-                    page: 1,
-                    pages: 1,
-                }
-            });
-            //pass an array cos the frontend is going to be expecting an array
-        }
-
-        if (selectedCuisines) {
-            const cuisinesArray = selectedCuisines
-                .split(",")
-                .map((cuisine) => new RegExp(cuisine, "i"));
-
-            query["cuisines"] = { $all: cuisinesArray }
-            //using the syntax becsue of the array
-        }
-
-        if (searchQuery) {
-            const searchRegex = new RegExp(searchQuery, "i");
-            query["$or"] = [
-                { restaurantName: searchRegex },
-                { cuisines: { $in: [searchRegex] } }
-            ]
-        }
-
-        const pageSize = 10;
-        const skip = (page - 1) * pageSize;
-
-        const restaurants = await Restaurant
-            .find(query)
-            .sort({ [sortOption]: 1 })
-            .skip(skip)
-            .limit(pageSize)
-            .lean();
-
-        const total = await Restaurant.countDocuments(query);
-
-        const response = {
-            data: restaurants,
-            pagination: {
-                total,
-                page,
-                pages: Math.ceil(total / pageSize),
-            }
-        }
-
-        res.json(response)
-
-
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({ message: 'Unable to search restaurant' })
+    query["city"] = new RegExp(city, "i");
+    const cityCheck = await Restaurant.countDocuments(query);
+    if (cityCheck === 0) {
+      return res.status(404).json({
+        data: [],
+        pagination: {
+          total: 0,
+          page: 1,
+          pages: 1,
+        },
+      });
+      //pass an array cos the frontend is going to be expecting an array
     }
-}
 
+    if (selectedCuisines) {
+      const cuisinesArray = selectedCuisines
+        .split(",")
+        .map((cuisine) => new RegExp(cuisine, "i"));
+
+      query["cuisines"] = { $all: cuisinesArray };
+      //using the syntax becsue of the array
+    }
+
+    if (searchQuery) {
+      const searchRegex = new RegExp(searchQuery, "i");
+      query["$or"] = [
+        { restaurantName: searchRegex },
+        { cuisines: { $in: [searchRegex] } },
+      ];
+    }
+
+    const pageSize = 10;
+    const skip = (page - 1) * pageSize;
+
+    const restaurants = await Restaurant.find(query)
+      .sort({ [sortOption]: 1 })
+      .skip(skip)
+      .limit(pageSize)
+      .lean();
+
+    const total = await Restaurant.countDocuments(query);
+
+    const response = {
+      data: restaurants,
+      pagination: {
+        total,
+        page,
+        pages: Math.ceil(total / pageSize),
+      },
+    };
+
+    res.json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Unable to search restaurant" });
+  }
+};
+
+const getRestaurantById = async (req: Request, res: Response) => {
+  try {
+    const restaurantId = req.params.restaurantId;
+    const restaurant = await Restaurant.findById(restaurantId);
+
+    if (!restaurant) {
+      return res.status(404).json({ message: "Restaurant not found" });
+    }
+
+    res.json(restaurant);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Unable to get restaurant",
+    });
+  }
+};
 
 export default {
-    searchRestaurant
-}
+  searchRestaurant,
+  getRestaurantById
+};
--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -1,19 +1,28 @@
-import express from "express"
-import { param } from "express-validator"
+import express from "express";
+import { param } from "express-validator";
 import { jwtCheck, jwtParse } from "../middleware/auth";
 import RestaurantController from "../controllers/RestaurantController";
 
 const router = express.Router();
 
-router.get("/search/:city",
-    param("city")
-        .isString()
-        .trim()
-        .notEmpty()
-        .withMessage("City parameter must be a valid string"),
-    RestaurantController.searchRestaurant
-
+router.get(
+  "/search/:city",
+  param("city")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("City parameter must be a valid string"),
+  RestaurantController.searchRestaurant
 );
 
+router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("RestaurantId parameter must be a valid string"),
+  RestaurantController.getRestaurantById
+);
 
 export default router;
